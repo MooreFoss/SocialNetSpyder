6f5536a05ec489078ab9b8cd3af15662d5dab040
MooreFoss <thelmh@outlook.com>
2024-10-14 14:42:58 +0800

    修复登录问题

--- routes/login.js
+++ routes/login.js
@@ -20,26 +20,19 @@ router.post('/', async (req, res) => {
       console.log('用户不存在');
       return res.status(400).json({ msg: '用户名或密码错误' });
     }
-    // console.log('用户数据:', user); // 打印用户数据
-    // console.log('输入的密码:', password); // 打印输入的密码
 
-    // 使用回调函数进行密码比较
-    bcrypt.compare(password, user.password, function (err, result) {
-      if (err) {
-        console.error('密码比较错误:', err);
-        return res.status(500).send('服务器错误');
-      }
-      if (result) {
-        console.log('密码正确');
-        const payload = { userId: user.id };
-        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
-        res.cookie('token', token, { httpOnly: true });
-        res.redirect('/manage');
-      } else {
-        console.log('密码错误');
-        return res.status(400).json({ msg: '用户名或密码错误' });
-      }
-    });
+    // 使用异步函数进行密码比较
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      // console.log('密码正确');
+      const payload = { userId: user.id };
+      const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+      res.cookie('token', token, { httpOnly: true });
+      res.redirect('/manage');
+    } else {
+      // console.log('密码错误');
+      return res.status(400).json({ msg: '用户名或密码错误' });
+    }
   } catch (err) {
     console.error(err.message);
     res.status(500).send('服务器错误');
