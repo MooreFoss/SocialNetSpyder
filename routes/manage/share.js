3519f486f58fc74f7365ccae22d6140efb48fac2
MooreFoss <thelmh@outlook.com>
2024-12-04 14:33:24 +0800

    引入mongoose库以支持MongoDB操作，准备进行数据模型的扩展

--- routes/manage/share.js
+++ routes/manage/share.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const Link = require('../../models/link');
 const Visit = require('../../models/visit');
 const Guest = require('../../models/guests');
